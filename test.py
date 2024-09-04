dc09187dd5d69a6e248dfdcadc846ad79aedcf31
DfraserWhitecliffe <20230682@mywhitecliffe.com>
2024-09-05 11:37:15 +1200

    Debugging and Refactoring
    Naming scheme seemed to fix things, trying to see if it still works with the removed if statement, and making the names all underscore

--- test.py
+++ test.py
@@ -6,34 +6,34 @@ class TestBowlingGame(unittest.TestCase):
     def setUp(self):
         self.game = bowling_game.BowlingGame()
 
-    def test_gutterGame(self):
+    def test_gutter_game(self):
         for i in range(0, 20):
             self.game.rolls(0)
         assert self.game.score()==0
 
-    def test_allOnes(self):
+    def test_all_0nes(self):
         self.rollMany(1, 20)
         assert self.game.score()==20
 
-    def test_oneSpare(self):
+    def test_one_spare(self):
         self.game.rolls(5)
         self.game.rolls(5)
         self.game.rolls(3)
         self.rollMany(0,17)
         assert self.game.score()==16
 
-    def test_oneStrike(self):
+    def test_one_strike(self):
         self.game.rolls(10)
         self.game.rolls(4)
         self.game.rolls(3)
         self.rollMany(0,16)
         assert  self.game.score()==24
 
-    def test_perfectGame(self):
+    def test_perfect_game(self):
         self.rollMany(10,12)
         assert self.game.score()==300
 
-    def test_oneSpare(self):
+    def test_one_spare(self):
         self.rollMany(5,21)
         assert self.game.score()==150
 
@@ -42,5 +42,3 @@ class TestBowlingGame(unittest.TestCase):
         for i in range(rolls):
             self.game.rolls(pins)
 
-if __name__ == '__main__':
-    unittest.main()
