57fdbaba469b043d7db49848b24b6298d55e9db4
DfraserWhitecliffe <20230682@mywhitecliffe.com>
2024-09-05 10:59:28 +1200

    Refactoring
    Added appropriate spacing between everything

--- bowling_game.py
+++ bowling_game.py
@@ -8,22 +8,29 @@ class BowlingGame:
     def score(self):
         result = 0
         rollIndex=0
+        
         for frameIndex in range(10):
+
             if frameIndex in range(10):
                 result += self.StrikeScore(rollIndex)
                 rollIndex +=1
+
             elif self.isSpare(rollIndex):
                 result += self.spareScore(rollIndex)
                 rollIndex +=2
+
             else:
                 result += self.frameScore(rollIndex)
             rollIndex +=2
+
             return result
 
     def isStrike(self, rollIndex):
         return self.rolls[rollIndex] == 10
+    
     def isSpare(self, rollIndex):
         return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
+    
     def stickeScore(self,rollIndex):
         return  10+ self.rolls[rollIndex+1]+ self.rolls[rollIndex+2]
 
